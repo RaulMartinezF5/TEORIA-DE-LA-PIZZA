093d73fc4a34d593d84d745e0fb2425254fe9ca0
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2022-11-10 11:45:55 +0100

    Add const

---
+++ app.js
@@ -0,0 +1,6 @@
+const harina=" 400 gr deharina de trigo ";
+const agua=" 200 ml de agua tibia ";
+const aceite=" 2 cucharadas de aceite de oliva ";
+const levadura=" 15 gr de levadura fresca ";
+const sal=" una pizca de sal";
+console.log(harina + agua + aceite + levadura + sal);
